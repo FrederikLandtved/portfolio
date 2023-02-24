d5e2a73bb18558551dd65ee93137daacf299a476
Frederik Landtved Jensen <frederiklandtved@hotmail.dk>
2023-02-24 15:48:00 +0100

    Added animating titles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  title = 'portfolio-frederik';
-  public typedText: string = 'Frederik Landtved';
+  name: string = 'Frederik Landtved';
+  changingText: string = '';
+  stringsToShow: string[] = ['Full stack developer', 'Angular developer', 'C# developer'];
+  currentStringIndex: number = 0;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.changeText();
+    this.changingText = this.stringsToShow[0];
+    setInterval(() => {
+      this.changeText();
+    }, 3000);
+  }
+
+  changeText() {
+    const element = document.querySelector('.changing-text') as HTMLElement;
+    element.classList.remove('show');
+    setTimeout(() => {
+      this.changingText = this.stringsToShow[this.currentStringIndex++ % this.stringsToShow.length];
+      element.classList.add('show');
+    }, 1000);
   }
 }
